5c5873fffd6356d1e6645507b68900eb537b8717
xuanvitran <xuan-vi@worldcandance.com>
2023-11-18 23:17:48 +0100

    add(content): about

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,1 @@
 import './style.css';
-// import { setupCounter } from './counter.ts';
-
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-
-// `;
-
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
